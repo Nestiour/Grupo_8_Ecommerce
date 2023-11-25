75a489223005461f40dadca7189f56dc9539cef4
Francisco Di Patti <panchodipatti@gmail.com>
2023-11-25 13:24:28 -0300

    agregando ruta para el detalle del producto

--- src/routes/main.js
+++ src/routes/main.js
@@ -6,5 +6,6 @@ router.get('/', mainController.Index);
 router.get('/login', mainController.Login);
 router.get('/cart', mainController.Cart);
 router.get('/register', mainController.Register);
+router.get('/productDetails', mainController.ProductDetails)
 
 module.exports = router;
