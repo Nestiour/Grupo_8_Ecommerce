fc88f994a084243e38254c92c377bf486f801c34
Nestor <nestnick0@gmail.com>
2023-12-06 19:55:30 -0300

    routes y controllers

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const productsDatabase = require('../data/productsDataBase.json');
+
 const mainControllers = {
+
     Index: (req, res) => {
-        res.render('home')
+        res.render('home', { products: productsDatabase });
     },
     Login: (req, res) => {
         res.render('login')
@@ -19,6 +24,67 @@ const mainControllers = {
     },
     Edit: (req, res) => {
         res.render('editarProducto')
+    },
+
+    CreateProduct: (req, res) => {
+        const { name, price, category, description } = req.body;
+        const image = req.file ? req.file.filename : null;
+
+        const newProduct = {
+            id: productsDatabase.length + 1,
+            name,
+            price,
+            category,
+            description,
+            image
+        };
+
+        productsDatabase.push(newProduct);
+
+        fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'), JSON.stringify(productsDatabase, null, 2));
+
+        res.redirect('/'); // Puedes redirigir a la página principal o a la lista de productos después de la creación.
+    },
+
+    EditProductPage: (req, res) => {
+        const productId = parseInt(req.params.id);
+        const product = productsDatabase.find(p => p.id === productId);
+        res.render('editarProducto', { product });
+    },
+
+    EditProduct: (req, res) => {
+        const productId = parseInt(req.params.id);
+        const { name, price, category, description } = req.body;
+        const image = req.file ? req.file.filename : null;
+
+        const updatedProduct = {
+            id: productId,
+            name,
+            price,
+            category,
+            description,
+            image
+        };
+
+        const index = productsDatabase.findIndex(p => p.id === productId);
+        productsDatabase[index] = updatedProduct;
+
+        fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'), JSON.stringify(productsDatabase, null, 2));
+
+        res.redirect('/');
+    },
+
+    DeleteProduct: (req, res) => {
+        const productId = parseInt(req.params.id);
+
+        const index = productsDatabase.findIndex(p => p.id === productId);
+        if (index !== -1) {
+            productsDatabase.splice(index, 1);
+
+            fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'), JSON.stringify(productsDatabase, null, 2));
+        }
+
+        res.redirect('/');
     }
 }
 
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainControllers');
+const multer = require('multer');
+const upload = multer({ dest: '../public/images' }); // Configura la carpeta de destino para subir archivos
 
+router.post('/crearProducto', upload.single('imagen'), mainController.CreateProduct);
+router.get('/editarProducto/:id', mainController.EditProductPage);
+router.post('/editarProducto/:id', upload.single('imagen'), mainController.EditProduct);
+router.get('/eliminarProducto/:id', mainController.DeleteProduct);
+
+//espacio//
 
 router.get('/', mainController.Index);
 router.get('/login', mainController.Login);
 router.get('/cart', mainController.Cart);
 router.get('/register', mainController.Register);
 
-router.get('/crearProducto', mainController.Add);
-router.get('/editarProducto', mainController.Edit);
 router.get('/productDetails', mainController.ProductDetails)
 
 module.exports = router;
